2675a3b27ae0fcedece8d7c75924e8f019d00c85
dan <humeniuc.dan.stefan@gmail.com>
2025-12-06 21:18:43 +0200

    pos enchanced deberta ner

--- consts.py
+++ consts.py
@@ -1,11 +1,18 @@
+import os
 from pathlib import Path
 
+__rootPath = Path.cwd()
+
 def __compute_cache_paths():
-    rootPath = Path.cwd()
+    paths = ["hf_cache", "data", "models"]
+
+    for path in paths:
+        os.makedirs(__rootPath.joinpath(path), exist_ok=True)
+
     return (
-        rootPath.joinpath("hf_cache"),
-        rootPath.joinpath("data"),
-        rootPath.joinpath("models")
+        __rootPath.joinpath("hf_cache"),
+        __rootPath.joinpath("data"),
+        __rootPath.joinpath("models")
     )
 
 HF_PATH, DATA_PATH, MODEL_PATH = __compute_cache_paths()
